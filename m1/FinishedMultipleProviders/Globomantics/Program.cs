15bd95fb73fe50a466b82e9f7651f3fb912ec6f0
Roland Guijt <roland.guijt@gmail.com>
2026-05-27 15:18:24 +0200

    Add comment to get google id and secret

--- m1/FinishedMultipleProviders/Globomantics/Program.cs
+++ m1/FinishedMultipleProviders/Globomantics/Program.cs
@@ -23,8 +23,9 @@ builder.Services.AddAuthentication(o =>
     {
         o.SignInScheme = 
             ExternalAuthenticationDefaults.AuthenticationScheme;
-        o.ClientId = "686977813024-d9i87jqqovj5tu5luks9rk8gl33ck3rb.apps.googleusercontent.com";
-        o.ClientSecret = "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";
+        //Please get your Google ClientId and Secret here: https://4sh.nl/googleclient
+        o.ClientId = "";
+        o.ClientSecret = "";
     });
 
 var app = builder.Build();
